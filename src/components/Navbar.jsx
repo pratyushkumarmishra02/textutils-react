ce23f04bd01a3a85f595612a5142a437f2480c55
PRATYUSA KUMAR MISHRA <pm490394@gmail.com>
2026-06-24 00:51:55 +0530

    updated navbar using useLocation and useEffect hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Link, NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar(props) {
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState("/");
+
+  useEffect(() => {
+    setCurrentPath(location.pathname);
+  }, [location]);
+
   return (
     <nav
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
@@ -11,9 +18,7 @@ export default function Navbar(props) {
         <Link className="navbar-brand" to="/">
           {props.title}
         </Link>
-        {/* <a className="navbar-brand" href="/">
-          {props.title}
-        </a> */}
+
         <button
           className="navbar-toggler"
           type="button"
@@ -26,81 +31,62 @@ export default function Navbar(props) {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div
+          className="collapse navbar-collapse"
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink
+              <Link
                 to="/"
-                end
-                className={({ isActive }) =>
-                  `nav-link ${isActive ? "active fw-bold text-primary" : ""}`
-                }
+                className={`nav-link ${
+                  currentPath === "/"
+                    ? "active fw-bold text-primary"
+                    : ""
+                }`}
               >
                 Home
-              </NavLink>
-              {/* <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a> */}
+              </Link>
             </li>
 
             <li className="nav-item">
-              <NavLink
+              <Link
                 to="/about"
-                className={({ isActive }) =>
-                  `nav-link ${isActive ? "active fw-bold text-primary" : ""}`
-                }
+                className={`nav-link ${
+                  currentPath === "/about"
+                    ? "active fw-bold text-primary"
+                    : ""
+                }`}
               >
                 {props.aboutText}
-              </NavLink>
-              {/* <a className="nav-link active" aria-current="page" href="#">
-                About
-              </a> */}
+              </Link>
             </li>
           </ul>
 
           <div className="d-flex">
             <div
               className="bg-primary rounded mx-2"
-              onClick={() => {
-                props.toggleMode("primary");
-              }}
+              onClick={() => props.toggleMode("primary")}
               style={{ height: "30px", width: "30px", cursor: "pointer" }}
             ></div>
+
             <div
               className="bg-danger rounded mx-2"
-              onClick={() => {
-                props.toggleMode("danger");
-              }}
+              onClick={() => props.toggleMode("danger")}
               style={{ height: "30px", width: "30px", cursor: "pointer" }}
             ></div>
+
             <div
               className="bg-warning rounded mx-2"
-              onClick={() => {
-                props.toggleMode("warning");
-              }}
+              onClick={() => props.toggleMode("warning")}
               style={{ height: "30px", width: "30px", cursor: "pointer" }}
             ></div>
+
             <div
               className="bg-success rounded mx-2"
-              onClick={() => {
-                props.toggleMode("success");
-              }}
+              onClick={() => props.toggleMode("success")}
               style={{ height: "30px", width: "30px", cursor: "pointer" }}
             ></div>
-            {/* <div
-              className="bg-light rounded mx-2"
-              onClick={() => {
-                props.toggleMode("light");
-              }}
-              style={{ height: "30px", width: "30px", cursor: "pointer" }}
-            ></div>
-            <div
-              className="bg-dark rounded mx-2"
-              onClick={() => {
-                props.toggleMode("dark");
-              }}
-              style={{ height: "30px", width: "30px", cursor: "pointer" }}
-            ></div> */}
           </div>
 
           <div
@@ -110,12 +96,11 @@ export default function Navbar(props) {
           >
             <input
               className="form-check-input"
-              onClick={() => {
-                props.toggleMode(null);
-              }}
+              onClick={() => props.toggleMode(null)}
               type="checkbox"
               id="flexSwitchCheckDefault"
             />
+
             <label
               className="form-check-label"
               htmlFor="flexSwitchCheckDefault"
@@ -123,18 +108,6 @@ export default function Navbar(props) {
               {props.text}
             </label>
           </div>
-
-          {/* <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-primary" type="submit">
-              Search
-            </button>
-          </form> */}
         </div>
       </div>
     </nav>
